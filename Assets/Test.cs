669f9fa89198b0655c86df06a0ad6570da832a29
Patrice Michaud <michaud.patrice@gmail.com>
2013-10-29 01:22:36 -0400

    no message

---
+++ Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour 
+{
+	[SerializeField]
+	private LLT.EMRoot _root;
+	
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		var rootObject = _root.DisplayTree.FindObject();
+		yield return new WaitForSeconds(1f);
+		
+		rootObject.AnimationHead.GotoAndPlay("green");
+		
+		yield return new WaitForSeconds(1f);
+		
+		rootObject.AnimationHead.GotoAndPlay("blue");
+		
+		yield return new WaitForSeconds(1f);
+		
+		rootObject.AnimationHead.GotoAndPlay("red");
+	}
+}
